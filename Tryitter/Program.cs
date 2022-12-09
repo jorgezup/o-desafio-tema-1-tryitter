c571040730fa814c895a5ce7d736d224e171e9c5
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-12-09 15:33:33 -0400

    chore: add health check and removed unused folders

--- Tryitter/Program.cs
+++ Tryitter/Program.cs
@@ -6,7 +6,7 @@ using Tryitter.WebApi;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -39,11 +39,13 @@ builder.Services.AddAuthentication(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+app.MapHealthChecks("/health");
+app.MapGet("/", () => "Seja bem vindo a Tryitter API");
 
 app.UseSwagger();
 app.UseSwaggerUI();
